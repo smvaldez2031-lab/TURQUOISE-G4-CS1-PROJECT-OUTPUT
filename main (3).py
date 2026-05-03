2baf6fe5deb01c71a950a8e07c77a640d626c1c8
Sebastian Meil C. Valdez <smvaldez2031@crc.pshs.edu.ph>
2026-05-03 22:00:36 +0800

    Refactor input prompts and display logic

--- main (3).py
+++ main (3).py
@@ -16,12 +16,12 @@ def Display(savings, days):
         per_day_save = savings / days
         print(f"You have saved {savings:.2f} peso(s).")
         print(f"You saved around {per_day_save:.2f} peso(s) saved per day.")
-name_1 = input("Enter your first name: ")
+name_1 = input("Please Enter your first name: ")
 while name_1 == "":
-    name_1 = input("Enter your first name: ")
-name_2 = input("Enter your last name: ")
+    name_1 = input("Please Enter your first name: ")
+name_2 = input("Please Enter your last name: ")
 while name_2 == "":
-    name_2 = input("Enter your last name: ")
+    name_2 = input("Please Enter your last name: ")
 print("Hello,", name_1, name_2)
 time = int(input("Please enter (1) if your budget is weekly, or (2) if it is monthly: "))
 while time == "":
@@ -30,10 +30,10 @@ if time<=0 or time >= 3:
     print("INVALID INPUT")
     quit()
 allowance = int(input("Enter your allowance: "))
-saved = int(input("Enter how much you saved on your budget: "))
-expense_1 = int(input("Enter how much you spent on transportation: "))
-expense_2 = int(input("Enter how much you spent on consumables: "))
-expense_3 = int(input("Enter how much you spent on other expenses: "))
+saved = int(input("Please Enter how much you saved on your budget: "))
+expense_1 = int(input("Please Enter how much you spent on transportation: "))
+expense_2 = int(input(" Please Enter how much you spent on consumables: "))
+expense_3 = int(input("Please Enter how much you spent on other expenses: "))
 if time == 1:
     savings = Compute(allowance, saved, expense_1, expense_2, expense_3, 7)
     Display(savings, time)
